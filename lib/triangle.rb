dfb45a51effd62064fab95873459b2d64d7c5ab8
Heather Kirk <hskirk@gmail.com>
2019-02-18 18:16:51 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,21 @@
 class Triangle
-  # write code here
+  attr_accessor :equilateral, :isosceles, :scalene
+  
+  def initialize(side1,side2,side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+  
+  def kind
+    if 
+      side1 == side2 && side2 == side3
+      @equilateral
+  
+  end
 end
+
+  class TriangleError < StandardError
+    "Not a triangle."
+  end 
+end 
